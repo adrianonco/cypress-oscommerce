c979e2d7f8b21972797297825c956f872d9a5e9a
Onco <adrianonco@users.noreply.github.com>
2024-02-11 22:26:09 +0100

    Added debugging config and Experimental Studio

--- cypress/e2e/spec1.cy.js
+++ cypress/e2e/spec1.cy.js
@@ -2,4 +2,27 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('Test1', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('https://sqademosatp.net/watch/');
+    cy.get('[data-id="28"] > .type-1 > .name > a').click();
+    cy.get('#btn-cart > .add-to-cart').click();
+    cy.get('.qty > .qty-box > .bigger').click();
+    cy.get('.right-buttons > .btn-2').click();
+    /* ==== End Cypress Studio ==== */
+  });
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('Test2', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('https://demo.oscommerce.com/watch/');
+    cy.get('#box-1857 > .products-listing > .slick-next').click();
+    cy.get('[data-id="25"] > .type-1 > .name > a').click();
+    cy.get('.add-to-cart').click();
+    cy.get('.qty > .qty-box > .bigger').click();
+    cy.get('.right-buttons > .btn-2').click();
+    /* ==== End Cypress Studio ==== */
+  });
 })
