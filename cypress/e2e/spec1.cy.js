42f0dea87895f9b3fc5b602a02fc43669b697e16
Onco <adrianonco@users.noreply.github.com>
2024-02-11 23:08:58 +0100

    Added Step 3: add to cart

--- cypress/e2e/spec1.cy.js
+++ cypress/e2e/spec1.cy.js
@@ -7,11 +7,25 @@ describe('OSCommerce Product Purchase Tests', () => {
         // Iterate over each product in the loaded fixture data
         data.products.forEach((product) =>  {
 
-            // Step 1: Go to the specific website calling the custom command with the name of the fixture file
+            // Step 1: Go to website
+            // Go to the specific website calling the custom command with the name of the fixture file
             cy.navigateToUrl('urls');
 
-            // Step 2: Find a clickable element that contains a given name in 'products' fixture
+            // Step 2: Click required product
+            // Find a clickable element that contains a given name in 'products' fixture
             cy.contains(product.name).click();
+
+            // Step 3: Add to Cart
+            // Close the pop-up at the bottom ir order to avoid double "Add to Basket" button
+            cy.get('.close').click();
+            // Click the "Add to Basket" button
+            // Click the 'Add to Basket' button that is shown in another pop-up
+            cy.get('#btn-cart > .add-to-cart').click();
+            /*
+            cy.get('.pop-up-content', { timeout: 10000 }).should('be.visible').and('contain', 'Item');
+            // Wait for the pop-up form to appear as it has delay
+            cy.wait(6000);
+            */
         });
     });
   });
