335840d5170cd68c3a8ca2c4ad5b5c6ffd2001b1
Onco <adrianonco@users.noreply.github.com>
2024-02-11 23:00:37 +0100

    Added Step 2: clicl specific product name

--- cypress/e2e/spec1.cy.js
+++ cypress/e2e/spec1.cy.js
@@ -9,6 +9,9 @@ describe('OSCommerce Product Purchase Tests', () => {
 
             // Step 1: Go to the specific website calling the custom command with the name of the fixture file
             cy.navigateToUrl('urls');
+
+            // Step 2: Find a clickable element that contains a given name in 'products' fixture
+            cy.contains(product.name).click();
         });
     });
   });
