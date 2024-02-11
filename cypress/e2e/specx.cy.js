5e64bbab14bddd7063f9f040df5a787c203c6b5a
Onco <adrianonco@users.noreply.github.com>
2024-02-11 22:46:27 +0100

    Created tests with Cypress Studio and renamed spec for them

--- cypress/e2e/specx.cy.js
+++ cypress/e2e/specx.cy.js
@@ -25,4 +25,30 @@ describe('template spec', () => {
     cy.get('.right-buttons > .btn-2').click();
     /* ==== End Cypress Studio ==== */
   });
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('Test3', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('https://demo.oscommerce.com/watch');
+    cy.get('#box-1857 > .products-listing > .slick-next').click();
+    cy.get('[data-id="25"] > .type-1 > .name > a').click();
+    cy.get('.close').click();
+    cy.get('#btn-cart > .add-to-cart').click();
+    cy.get('.qty > .qty-box > .bigger').click();
+    cy.get('.right-buttons > .btn-2').click();
+    cy.get('.strong').click();
+    /* ==== End Cypress Studio ==== */
+  });
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('Test4', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('https://sqademosatp.net/watch/');
+    cy.get('[data-id="28"] > .type-1 > .name > a').click();
+    cy.get('.close').click();
+    cy.get('#btn-cart > .add-to-cart').click();
+    cy.get('.qty > .qty-box > .bigger').click();
+    cy.get('.right-buttons > .btn-2').click();
+    /* ==== End Cypress Studio ==== */
+  });
 })
